7b3bf456e3fd92a0e60a5280d128f1475d759dfd
Ansheng Zhou <azhou@lcs.on.ca>
2024-11-05 20:43:48 -0500

    Initial Commit

---
+++ ChartExercise/ChartExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartExerciseApp.swift
+//  ChartExercise
+//
+//  Created by Ansheng Zhou on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct ChartExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
